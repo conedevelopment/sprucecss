7e092ece074df8c6a21a6999b3ab3b708b215d16
Adam Laki <info@adamlaki.com>
2022-07-29 18:56:57 +0200

    Preview: change cookies to localStorage

--- preview/assets/js/reading-direction.js
+++ preview/assets/js/reading-direction.js
@@ -1,7 +1,4 @@
-import Cookie from '../../../js/utils/cookie.js';
-const cookie = new Cookie();
-
 document.getElementById('reading-direction').addEventListener('input', function (event) {
   document.documentElement.setAttribute('dir', event.target.value);
-  cookie.set('spruce-reading-direction', event.target.value, 31556926, '/');
+  localStorage.setItem('spruce-reading-direction', event.target.value, 31556926, '/');
 }, false);
--- preview/assets/js/theme-mode.js
+++ preview/assets/js/theme-mode.js
@@ -1,7 +1,4 @@
-import Cookie from '../../../js/utils/cookie.js';
-const cookie = new Cookie();
-
 document.getElementById('theme-mode').addEventListener('input', function (event) {
   document.documentElement.setAttribute('data-theme-mode', event.target.value);
-  cookie.set('spruce-theme-mode', event.target.value, 31556926, '/');
+  localStorage.setItem('spruce-theme-mode', event.target.value, 31556926, '/');
 }, false);
